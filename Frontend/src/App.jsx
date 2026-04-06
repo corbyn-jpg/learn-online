da54a63a5e62279604633ecbe6268484cdfc272b
corbyn-jpg <159898463+corbyn-jpg@users.noreply.github.com>
2026-04-06 20:37:14 +0200

    Added React routing using the menu
    Added routing to the menu to create the page routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./components/menu";
 import SideMenu from "./components/sideMenu";
 import Dashboard from "./pages/dashboard";
+import CalendarPage from "./pages/calendar";
 
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Menu />
       <SideMenu />
       <main className="pt-24 pl-40 pr-4">
-        <Dashboard />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/calendar" element={<CalendarPage />} />
+        </Routes>
       </main>
-    </>
+    </BrowserRouter>
   );
 }
 
